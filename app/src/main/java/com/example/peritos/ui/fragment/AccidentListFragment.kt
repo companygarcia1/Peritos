0877681983a70261b81c935addd55882f13cf280
Jose Compañy <>
2023-12-05 13:35:33 +0100

    show detail accident

--- app/src/main/java/com/example/peritos/ui/fragment/AccidentListFragment.kt
+++ app/src/main/java/com/example/peritos/ui/fragment/AccidentListFragment.kt
@@ -5,9 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import androidx.fragment.app.Fragment
 import com.example.peritos.databinding.FragmentAccindentListBinding
 import com.example.peritos.model.DataSource
+import com.example.peritos.model.accident.Accident
+import com.example.peritos.model.vehicle.Vehicle
+import com.example.peritos.ui.NavigationManager
 import com.example.peritos.ui.adapters.AccidentListAdapter
 
 class AccidentListFragment : Fragment() {
@@ -36,7 +40,14 @@ class AccidentListFragment : Fragment() {
     }
 
     private fun setListener() {
-
+        binding.listview.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, view, position, id ->
+                val item = adapter.getItem(position)
+                if (item is Accident) {
+                    val vehiculo: Accident = item
+                    NavigationManager.openAccidentDetail(ctx, vehiculo.id)
+                }
+            }
     }
 
     private fun configView() {
